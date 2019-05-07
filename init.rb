aea3e8d57f5b92570958118c2a9d427b98895ec6
bilel-kedidi <bilel.kedidi@gmail.com>
2019-05-07 03:54:21 +0100

    fix style

--- init.rb
+++ init.rb
@@ -7,4 +7,20 @@ Redmine::Plugin.register :redmine_context_menu_issues do
   author_url 'https://www.github.com/bilel-kedidi'
 
   require 'issues_context_menu_issues_hook'
+
+  begin
+    directory = File.join(Rails.root, 'plugins', 'redmine_context_menu_issues', 'assets', 'stylesheets')
+    source_files = Dir[directory + "/*"]
+    destination = File.join(Rails.root, 'public')
+    source_files.each do |file|
+      begin
+        target = destination
+        unless File.exist?(target) && FileUtils.identical?(file, target)
+          FileUtils.cp(file, target)
+        end
+      rescue Exception => e
+        raise "Could not copy #{file} to #{target}: " + e.message
+      end
+    end
+  end
 end
