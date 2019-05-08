52a9d90aa5a103ef0a0f9ad2e72fdba5802a4e48
bilel-kedidi <bilel.kedidi@gmail.com>
2019-05-08 11:30:17 +0100

    fix

--- init.rb
+++ init.rb
@@ -8,19 +8,19 @@ Redmine::Plugin.register :redmine_context_menu_issues do
 
   require 'issues_context_menu_issues_hook'
 
-  begin
-    directory = File.join(Rails.root, 'plugins', 'redmine_context_menu_issues', 'assets', 'stylesheets')
-    source_files = Dir[directory + "/*"]
-    destination = File.join(Rails.root, 'public')
-    source_files.each do |file|
-      begin
-        target = destination
-        unless File.exist?(target) && FileUtils.identical?(file, target)
-          FileUtils.cp(file, target)
-        end
-      rescue Exception => e
-        raise "Could not copy #{file} to #{target}: " + e.message
-      end
-    end
-  end
+  # begin
+  #   directory = File.join(Rails.root, 'plugins', 'redmine_context_menu_issues', 'assets', 'stylesheets')
+  #   source_files = Dir[directory + "/*"]
+  #   destination = File.join(Rails.root, 'public')
+  #   source_files.each do |file|
+  #     begin
+  #       target = destination
+  #       unless File.exist?(target) && FileUtils.identical?(file, target)
+  #         FileUtils.cp(file, target)
+  #       end
+  #     rescue Exception => e
+  #       raise "Could not copy #{file} to #{target}: " + e.message
+  #     end
+  #   end
+  # end
 end
